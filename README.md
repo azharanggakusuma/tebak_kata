0da59027f2b222b3a6ca39235eb40b7be2c047eb
Azharangga Kusuma <97172969+azharanggakusuma@users.noreply.github.com>
2023-08-16 11:23:42 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Penjelasan Singkat Tentang Permainan Ini
+
+### Permainan Tebak Kata
+Pada project ini, saya membuat permainan ini untuk keperluan mengikuti lomba 17an yang diselenggarakan oleh @code.kyra x @vipcodestudio dan bekerja sama dengan WPU sebagai community partner. Saya membuat permainan ini menggunakan javascript. Permainan ini juga sudah responsif❗️
+
+### Cara bermain
+Saat permainan dimulai, kamu akan diberikan sebuah prompt kotak untuk menebak dan diberikan pilihan untuk memasukkan huruf sebanyak 5-8 kali. Untuk membantu kamu menebak kata, kamu dapat mengklik petunjuk untuk bantuan. Jika kamu benar atau salah, sebuah pesan akan ditampilkan. Jika kamu ingin mengubah kata apa pun, cukup klik Lanjutkan. 
+
+# Screenshot
+Berikut adalah screenshot project saya:
+
+### Tampilan Dekstop:
+![screenshot](assets/img/dekstop.png) 
+
+### Tampilan Mobile:
+![screenshot](assets/img/mobile.png)
+
+# Akhir Kata
+Dikembangkan oleh:
+([@azharangga_kusuma](https://instagram.com/azharangga_kusuma))
---
+++ assets/db/words-db.js
@@ -0,0 +1,338 @@
+let wordList = [
+  {
+    word: "pisang",
+    hint: "buah berwarna kuning",
+  },
+  {
+    word: "permen",
+    hint: "makanan manis",
+  },
+  {
+    word: "github",
+    hint: "platform hosting kode",
+  },
+  {
+    word: "png",
+    hint: "format file gambar",
+  },
+  {
+    word: "perak",
+    hint: "logam abu-abu putih yang berharga",
+  },
+  {
+    word: "ponsel",
+    hint: "perangkat elektronik",
+  },
+  {
+    word: "gpu",
+    hint: "komponen komputer",
+  },
+  {
+    word: "java",
+    hint: "bahasa pemrograman",
+  },
+  {
+    word: "google",
+    hint: "mesin pencari terkenal",
+  },
+  {
+    word: "venesia",
+    hint: "kota terkenal di atas air",
+  },
+  {
+    word: "excel",
+    hint: "produk Microsoft untuk Windows",
+  },
+  {
+    word: "menari",
+    hint: "bergerak mengikuti musik",
+  },
+  {
+    word: "elang",
+    hint: "burung pemangsa",
+  },
+  {
+    word: "anggur",
+    hint: "buah kecil",
+  },
+  {
+    word: "madu",
+    hint: "sirup manis",
+  },
+  {
+    word: "selai",
+    hint: "campuran buah yang bisa disebarkan",
+  },
+  {
+    word: "jeruk nipis",
+    hint: "buah asam",
+  },
+  {
+    word: "semangka",
+    hint: "buah manis",
+  },
+  {
+    word: "zaitun",
+    hint: "buah kecil",
+  },
+  {
+    word: "persik",
+    hint: "buah lembut",
+  },
+  {
+    word: "ratu",
+    hint: "penguasa perempuan",
+  },
+  {
+    word: "kelinci",
+    hint: "hewan berbulu",
+  },
+  {
+    word: "laba-laba",
+    hint: "serangga delapan kaki",
+  },
+  {
+    word: "harimau",
+    hint: "kucing besar",
+  },
+  {
+    word: "paman",
+    hint: "saudara laki-laki ayah",
+  },
+  {
+    word: "virus",
+    hint: "penyakit",
+  },
+  {
+    word: "air",
+    hint: "cairan bening",
+  },
+  {
+    word: "xenon",
+    hint: "unsur kimia",
+  },
+  {
+    word: "yacht",
+    hint: "kapal",
+  },
+  {
+    word: "roti",
+    hint: "makanan panggang",
+  },
+  {
+    word: "keju",
+    hint: "produk susu",
+  },
+  {
+    word: "keledai",
+    hint: "kerabat kuda",
+  },
+  {
+    word: "eggnog",
+    hint: "minuman liburan",
+  },
+  {
+    word: "kulkas",
+    hint: "perangkat rumah tangga",
+  },
+  {
+    word: "gitar",
+    hint: "alat musik berdawai",
+  },
+  {
+    word: "hoki",
+    hint: "olahraga es",
+  },
+  {
+    word: "iglo",
+    hint: "rumah salju",
+  },
+  {
+    word: "jaket",
+    hint: "pakaian",
+  },
+  {
+    word: "teko",
+    hint: "pemanas air",
+  },
+  {
+    word: "daun",
+    hint: "bagian pohon",
+  },
+  {
+    word: "belalang",
+    hint: "serangga",
+  },
+  {
+    word: "mie",
+    hint: "pasta",
+  },
+  {
+    word: "jeruk",
+    hint: "buah jeruk",
+  },
+  {
+    word: "lada",
+    hint: "bumbu",
+  },
+  {
+    word: "mysql",
+    hint: "sistem basis data relasional",
+  },
+  {
+    word: "iran",
+    hint: "nama negara berkembang",
+  },
+  {
+    word: "flute",
+    hint: "alat musik",
+  },
+  {
+    word: "kripto",
+    hint: "berhubungan dengan cryptocurrency",
+  },
+  {
+    word: "tesla",
+    hint: "satuan kerapatan fluks magnet",
+  },
+  {
+    word: "mars",
+    hint: "planet dalam tata surya kita",
+  },
+  {
+    word: "proxy",
+    hint: "berhubungan dengan aplikasi server",
+  },
+  {
+    word: "email",
+    hint: "berhubungan dengan pertukaran pesan",
+  },
+  {
+    word: "html",
+    hint: "bahasa markup untuk web",
+  },
+  {
+    word: "udara",
+    hint: "berhubungan dengan gas",
+  },
+  {
+    word: "ide",
+    hint: "pemikiran atau saran",
+  },
+  {
+    word: "server",
+    hint: "berhubungan dengan komputer atau sistem",
+  },
+  {
+    word: "svg",
+    hint: "format gambar vektor",
+  },
+  {
+    word: "jpeg",
+    hint: "format file gambar",
+  },
+  {
+    word: "cari",
+    hint: "tindakan untuk menemukan sesuatu",
+  },
+  {
+    word: "kunci",
+    hint: "potongan kecil logam",
+  },
+  {
+    word: "mesir",
+    hint: "nama negara",
+  },
+  {
+    word: "joker",
+    hint: "film thriller psikologis",
+  },
+  {
+    word: "dubai",
+    hint: "nama negara maju",
+  },
+  {
+    word: "foto",
+    hint: "representasi orang atau pemandangan",
+  },
+  {
+    word: "nila",
+    hint: "sungai terbesar di dunia",
+  },
+  {
+    word: "hujan",
+    hint: "berhubungan dengan air",
+  },
+  {
+    word: "python",
+    hint: "bahasa pemrograman",
+  },
+  {
+    word: "gitar",
+    hint: "alat musik",
+  },
+  {
+    word: "tujuan",
+    hint: "suatu maksud atau niat",
+  },
+  {
+    word: "venus",
+    hint: "planet dalam tata surya kita",
+  },
+  {
+    word: "emas",
+    hint: "logam mulia berwarna kuning",
+  },
+  {
+    word: "ebay",
+    hint: "situs belanja online",
+  },
+  {
+    word: "golang",
+    hint: "bahasa pemrograman",
+  },
+  {
+    word: "koding",
+    hint: "berhubungan dengan pemrograman",
+  },
+  {
+    word: "matrix",
+    hint: "film fiksi ilmiah",
+  },
+  {
+    word: "bug",
+    hint: "berhubungan dengan pemrograman",
+  },
+  {
+    word: "avatar",
+    hint: "film fiksi ilmiah epik",
+  },
+  {
+    word: "gif",
+    hint: "format file gambar",
+  },
+  {
+    word: "mental",
+    hint: "berhubungan dengan pikiran",
+  },
+  {
+    word: "peta",
+    hint: "diagram representasi suatu area",
+  },
+  {
+    word: "pulau",
+    hint: "tanah dikelilingi air",
+  },
+  {
+    word: "hoki",
+    hint: "permainan outdoor terkenal",
+  },
+  {
+    word: "catur",
+    hint: "berhubungan dengan permainan dalam ruangan",
+  },
+  {
+    word: "viber",
+    hint: "aplikasi media sosial",
+  },
+];
